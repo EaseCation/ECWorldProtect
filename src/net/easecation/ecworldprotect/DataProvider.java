826fcbb6471a345dd3bdd97c2c0eed079d234a97
PeratX <1215714524@qq.com>
2016-12-08 20:27:14 +0800

    dataprovider: base methods part 1

--- src/net/easecation/ecworldprotect/DataProvider.java
+++ src/net/easecation/ecworldprotect/DataProvider.java
@@ -1,7 +1,47 @@
 package net.easecation.ecworldprotect;
 
+import cn.nukkit.level.Level;
+import cn.nukkit.utils.Config;
+
+import java.util.LinkedHashMap;
+
 /**
- * @author Administrator
+ * @author PeratX
  */
 public class DataProvider {
+    private Config conf;
+    private LinkedHashMap<String, Object> config;
+
+    public DataProvider(String file){
+        this.conf = new Config(file, Config.YAML);
+        this.config = (LinkedHashMap<String, Object>)this.conf.getAll();
+    }
+
+    public void save(){
+        this.conf.setAll(this.config);
+        this.conf.save();
+    }
+
+    public int getPermByName(String name){
+        switch (name){
+            case "0":
+            case "none":
+                return ECWorldProtect.WORLD_PERM_NONE;
+            case "1":
+            case "view":
+                return ECWorldProtect.WORLD_PERM_VIEW;
+            case "2":
+            case "edit":
+                return ECWorldProtect.WORLD_PERM_EDIT;
+            default:
+                return ECWorldProtect.WORLD_PERM_NONE;
+        }
+    }
+
+    public boolean addLevel(Level level, int defaultPerm){
+        if(this.config.containsValue(level.getFolderName())){
+
+        }
+        return true;
+    }
 }
--- src/net/easecation/ecworldprotect/ECWorldProtect.java
+++ src/net/easecation/ecworldprotect/ECWorldProtect.java
@@ -1,14 +1,45 @@
 package net.easecation.ecworldprotect;
 
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
 
+/**
+ * @author PeratX
+ */
 class ECWorldProtect extends PluginBase{
-    private Config config;
+    public static int WORLD_PERM_NONE = 0;
+    public static int WORLD_PERM_VIEW = 1;
+    public static int WORLD_PERM_EDIT = 2;
+
+    private DataProvider dataProvider;
 
     @Override
     public void onEnable() {
+        this.dataProvider = new DataProvider(this.getDataFolder() + "config.yml");
         this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
-        this.getLogger().notice("ECWorldProject has been enabled.");
+        this.getLogger().info(TextFormat.GREEN + "ECWorldProject has been enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        this.dataProvider.save();
+        this.getLogger().info(TextFormat.RED + "ECWorldProtect has been disabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getName()){
+            case "ecwp":
+                if(!command.testPermission(sender)){
+                    break;
+                }
+                if(args.length < 3){
+                    return false;
+                }
+                break;
+        }
+        return true;
     }
 }
